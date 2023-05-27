c58b24a2f484684b05590d417a59aeaf1ec6e55b
Nghia <nghiaku1905@gmail.com>
2023-05-27 11:13:31 +0700

    add ServletLogout

---
+++ src/main/java/com/code/alphavn/controller/userController/ServletLogout.java
@@ -0,0 +1,33 @@
+package com.code.alphavn.controller.userController;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet( name = "ServletLogout", value = "/logout")
+public class ServletLogout extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession();
+        session.removeAttribute("acc");
+//        request.getRequestDispatcher("/components/userComponents/home.jsp").forward(request, response); //test
+        response.sendRedirect("home");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
